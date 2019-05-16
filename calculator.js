e9b0a66aa8d2207f5677d06c69b796e82e952deb
Ruslan-Makeev <dec32m@gmail.com>
2019-05-17 00:35:28 +1200

    new calculator fith function constructor

---
+++ calculator.js
@@ -0,0 +1,24 @@
+
+//Display activate
+var dataInput = document.getElementById('dataInput');
+function pushBtn(obj) {
+    var pushed = obj.innerHTML;
+    //Using function constructor to replace see if it works...
+    var sum = new Function('a','b','return a + b');
+    var min = new Function('a','b','return a - b');
+    var mul = new Function('a','b','return a * b');
+    var div = new Function('a','b','return a / b');
+    if (pushed == '=') {
+        // Calculate
+        dataInput.innerHTML = /*eval*/(dataInput.innerHTML);
+    } else if (pushed == 'AC') {
+        // All Clear
+        dataInput.innerHTML = '0';
+    } else {
+        if (dataInput.innerHTML == '0') {
+            dataInput.innerHTML = pushed;
+        } else {
+            dataInput.innerHTML += pushed;
+        }
+    }
+}
