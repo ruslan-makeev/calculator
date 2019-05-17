a2bb29b2fb980f8eee70142d471e5869ea7f3103
Ruslan-Makeev <dec32m@gmail.com>
2019-05-17 19:30:24 +1200

    Finally completed the damn thing... Also added burning buttons look - kinda how I feel now =) -

--- calculator.js
+++ calculator.js
@@ -1,24 +1,41 @@
 
-//Display activate
+var operator;
+var firstValue;
+var secondValue;
 var dataInput = document.getElementById('dataInput');
 function pushBtn(obj) {
     var pushed = obj.innerHTML;
-    //Using function constructor to replace see if it works...
-    var sum = new Function('a','b','return a + b');
-    var min = new Function('a','b','return a - b');
-    var mul = new Function('a','b','return a * b');
-    var div = new Function('a','b','return a / b');
     if (pushed == '=') {
-        // Calculate
-        dataInput.innerHTML = FUNCTION CONSTRUCTOR (dataInput.innerHTML);
-    } else if (pushed == 'AC') {
-        // All Clear
+        secondValue = dataInput.innerHTML
+        dataInput.innerHTML = calculate(firstValue, operator, secondValue);
+    }
+    else if (pushed == '+' || pushed == '-' || pushed == '*' || pushed == '/') {
+        operator = pushed;
+        firstValue = dataInput.innerHTML;
+        dataInput.innerHTML = '';
+    }
+    else if (pushed == 'Del') {
         dataInput.innerHTML = '0';
-    } else {
+    }
+    else {
         if (dataInput.innerHTML == '0') {
             dataInput.innerHTML = pushed;
-        } else {
+        }
+        else {
             dataInput.innerHTML += pushed;
         }
     }
+}
+const calculate = (n1, operator, n2) => {
+    let result = '';
+    if (operator === '+') {
+        result = parseFloat(n1) + parseFloat(n2)
+    } else if (operator === '-') {
+        result = parseFloat(n1) - parseFloat(n2)
+    } else if (operator === '*') {
+        result = parseFloat(n1) * parseFloat(n2)
+    } else if (operator === '/') {
+        result = parseFloat(n1) / parseFloat(n2)
+    }
+    return result;
 }
